46f6adf311699de4d6f56deb02ec92e2faa4c47a
Vlad Zubin <zoobeen1@gmail.com>
2022-05-04 22:20:34 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+//functions
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function setColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+function onStartClick() {
+  intervalId = setInterval(setColor, 1000);
+  btnStart.disabled = true;
+}
+function onStopClick() {
+  btnStart.disabled = false;
+  clearInterval(intervalId);
+}
 
+//vars
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = 0;
+//events
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
