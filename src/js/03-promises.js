3270178bc6ae669c59994cd85f70468aa877702c
Vlad Zubin <zoobeen1@gmail.com>
2022-05-10 19:25:48 +0300

    final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+//variables
+const form = document.querySelector('form');
+const firstDelay = form.elements['delay'];
+const delayStep = form.elements['step'];
+const amount = form.elements['amount'];
+const button = document.querySelector('button');
+
+//functions
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position: position, delay: delay });
+      } else {
+        // Reject
+        reject({ position: position, delay: delay });
+      }
+    }, delay);
+  });
+}
+function onSubmit(e) {
+  e.preventDefault();
+  let setDelay = Number(firstDelay.value);
+  for (let i = 1; i <= Number(amount.value); i++) {
+    if (i > 1) {
+      setDelay += Number(delayStep.value);
+    }
+    createPromise(i, setDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+//main code
+button.addEventListener('click', onSubmit);
